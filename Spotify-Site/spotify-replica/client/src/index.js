a36579ab7069fddc2e087af81abae898316a3f42
Thomas Carriero <tcarriero@unc.edu>
2023-07-07 16:23:23 -0400

    spotify login authentication button added

---
+++ Spotify-Site/spotify-replica/client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Login';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
